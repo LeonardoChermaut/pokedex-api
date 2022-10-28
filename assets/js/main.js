1971b89b8bf961078bdc54478f9088f4ca93aa64
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2022-10-28 14:10:15 -0300

    requests map and converter json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
+function convertPokemonTypeToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon){
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#00${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-        <li class="type">grass</li>
+        <li class="type">${convertPokemonTypeToLi(pokemon.types).join('')}</li>
         <li class="type">poison</li>
         </ol>
-      <img src="https://seeklogo.com/images/B/bulbasaur-logo-8A27924C02-seeklogo.com.png" alt="${pokemon.name}"/>
+      <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"/>
     </div>
   </li>`
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,17 +1,17 @@
-const pokeApi = {}
+const pokeApi = {};
+
+pokeApi.getPokemonsDetails = (pokemon) =>{
+    return fetch(pokemon.url).then((response) =>response.json());
+}
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-   return fetch(url)
+    
+    return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
+    .then((detailRequests) => Promise.all((detailRequests)))
+    .then((pokemonsDetails) => pokemonsDetails)
+    .catch((error) => console.log(error));
 }
-
-Promisse.All([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-])
-.then((results) => {console.log(results);})
