7ba2341ff01e56fb187099e9b250b0872bc86861
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2022-10-28 13:39:11 -0300

    implements method map on PokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(res){
-    console.log(res);
-})
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+    <div class="detail">
+        <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+        </ol>
+      <img src="https://seeklogo.com/images/B/bulbasaur-logo-8A27924C02-seeklogo.com.png" alt="${pokemon.name}"/>
+    </div>
+  </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+       pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+            });
+    
